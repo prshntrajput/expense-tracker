d1031914475ac1b16c385b677f2c3ad03bfd65e5
Prashant Rajput <prshant.work@gmail.com>
2024-05-05 20:56:50 +0530

    signup page edited

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -38,8 +38,7 @@ export default function Page() {
         </h1>
 
         <p className="mt-4 leading-relaxed text-gray-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+          Track your Expenses
         </p>
            <SignIn path="/sign-in" />
        
